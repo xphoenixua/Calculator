da0462bfb7b44a8b13f64221c2210352eda1104c
xphoenixua <uzya03@gmail.com>
2020-12-23 10:12:18 +0200

    beta 2.0

--- Multifunctional_calculator.py
+++ Multifunctional_calculator.py
@@ -43,7 +43,7 @@ def subtract(string):
                 equ = equ.replace(old, new)
         else:
             flag = False
-    print(equ, 'equ minus')
+    print(equ, 'equ minus')#
     return equ
  
 def multiply(string):
